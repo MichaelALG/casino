95f0811085c9535fd11ccf73762f1b83fb9dad6a
MichaelALG <miguellagosg@gmail.com>
2026-03-12 13:35:26 -0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    // Advertencia: Esto permite que la compilación continúe incluso si
-    // hay errores de ESLint. Útil para demos rápidos.
-    ignoreDuringBuilds: true,
-  },
   typescript: {
     // Advertencia: Esto permite la compilación si hay errores de TS.
     ignoreBuildErrors: true,
